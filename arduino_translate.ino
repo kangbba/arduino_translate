fda66c97605d7861c2bee070c7f90b3665b48c2b
kangbba <sayneinteractive@gmail.com>
2023-02-06 02:04:28 +0900

    changed to ble

--- arduino_translate.ino
+++ arduino_translate.ino
@@ -1,65 +1,209 @@
-//공통
+/*
+    Video: https://www.youtube.com/watch?v=oCMOYS71NIU
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+
+   Create a BLE server that, once we receive a connection, will send periodic notifications.
+   The service advertises itself as: 6E400001-B5A3-F393-E0A9-E50E24DCCA9E
+   Has a characteristic of: 6E400002-B5A3-F393-E0A9-E50E24DCCA9E - used for receiving data with "WRITE" 
+   Has a characteristic of: 6E400003-B5A3-F393-E0A9-E50E24DCCA9E - used to send data with  "NOTIFY"
+
+   The design of creating the BLE server is:
+   1. Create a BLE Server
+   2. Create a BLE Service
+   3. Create a BLE Characteristic on the Service
+   4. Create a BLE Descriptor on the characteristic
+   5. Start the service.
+   6. Start advertising.
+
+   In this example rxValue is the data received (only accessible inside that function).
+   And txValue is the data to be sent, in this example just a byte incremented every second. 
+*/
 #include "BluetoothSerial.h"
 #include <SPI.h>
 #include <Wire.h>
 //U8G2
 #include <U8g2lib.h>
 #include <Arduino.h>
+
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE, 22, 21);
 
 //Bluetooth
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
-BluetoothSerial SerialBT;
 
-int previousLangCode = -1;
 
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint8_t txValue = 0;
+
+int previousCode = -1;
+int nowCode = 0;
+String recentMessage = "";
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+// #define SERVICE_UUID           "2ef826d2-a48d-11ed-a8fc-0242ac120002" // UART service UUID
+// #define CHARACTERISTIC_UUID_RX "2ef82e8e-a48d-11ed-a8fc-0242ac120002"
+// #define CHARACTERISTIC_UUID_TX "2ef83078-a48d-11ed-a8fc-0242ac120002"
+#define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "beb5483e-36e1-4688-b7f5-ea07361b26a8" // 
+#define CHARACTERISTIC_UUID_TX "beb5483e-36e1-4688-b7f5-ea07361b26a9" // 
+// RX (Receive) UUID는 아두이노로부터 모바일로의 데이터 수신에 사용되는 UUID이고, TX (Transmit) UUID는 모바일에서 아두이노로의 데이터 전송에 사용되는 UUID입니다.
+//MAC ADRESS 사용해서 데이터전달되는지?
+
+// CHARACTERISTIC_UUID_TX를 통해 데이터를 보내야 합니다.
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+      recentMessage = String(rxValue.c_str());
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+        for (int i = 0; i < rxValue.length(); i++)
+          Serial.print(rxValue[i]);
+
+        Serial.println();
+        Serial.println("*********");
+        nowCode++;
+      }
+    }
+};
+
+
+
+
+void initBLEDevice()
+{
+  // Create the BLE Device
+  BLEDevice::init("CANDY3");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+ // pServer->setMtu(256);
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pTxCharacteristic = pService->createCharacteristic(
+										CHARACTERISTIC_UUID_TX,
+										BLECharacteristic::PROPERTY_NOTIFY
+									);
+                      
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+											 CHARACTERISTIC_UUID_RX,
+											BLECharacteristic::PROPERTY_WRITE
+										);
+
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  pServer->getAdvertising()->start();
+  Serial.println("Waiting a client connection to notify...");
+}
 void setup() {
   Serial.begin(115200);
-
+  // Create the BLE Device
+  initBLEDevice();
   //Initialize U8G2 
   u8g2.begin();
   u8g2.enableUTF8Print();
   u8g2.setFontDirection(0);
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_unifont_t_korean1);
-  Message("U8G2 START!!");
+  Message("Translate Start!");
+}
+void loop() {
+  if (deviceConnected) {
+      pTxCharacteristic->setValue(&txValue, 1);
+      pTxCharacteristic->notify();
+      txValue++;
+  delay(10); // bluetooth stack will go into congestion, if too many packets are sent
+  }
 
-  //Initialize Bluetooth
-  SerialBT.begin("ESP32hyehwa"); //Bluetooth device name
-  Serial.println("블루투스 시작됨");
+  // disconnecting
+  if (!deviceConnected && oldDeviceConnected) {
+      delay(500); // give the bluetooth stack the chance to get things ready
+      pServer->startAdvertising(); // restart advertising
+      Serial.println("start advertising");
+      oldDeviceConnected = deviceConnected;
+  }
+  // connecting
+  if (deviceConnected && !oldDeviceConnected) {
+  // do stuff here on connecting
+      oldDeviceConnected = deviceConnected;
+  }
+  previousCode = nowCode;
+  Serial.println(recentMessage);
+  String beforeFullMsg = Split(recentMessage, ';', 0);
+  /////// 국가코드 READ 후 국가코드에 맞는 폰트를 설정한다 /////// 
+  int langCodeInt = Split(beforeFullMsg, ':', 0).toInt(); 
+
+  ChangeUTF(langCodeInt);
+  /////// 메세지 READ 후 출력한다 ///////
+  String finalMsg = Split(beforeFullMsg, ':', 1);
+  Message(recentMessage);
+  delay(20);
+}
+String Split(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
+void Message(String msg){
 
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-    Serial.println(Serial.readString());
+  u8g2.clearBuffer();
+  int lineLength = 15;
+  int len = msg.length();
+  int lines = len / lineLength + 1;
+  int height = 10;
+  for (int i = 0; i < lines; i++) {
+  int end = min((i + 1) * lineLength, len);
+  String line = msg.substring(i * lineLength, end);
+  u8g2.drawStr(0, height, line.c_str());
+  height += 10;
   }
-  
-  if (SerialBT.available()) {
-    String fullMsg = SerialBT.readString();
-    Serial.println(fullMsg);
 
-    //메세지 형식 => 12:Hello World; 
-    //오류시 12:Hello World;12:Hello World;12:Hello World 이런식의 메세지 출력됨.
-    //오류방지로 첫 ; 까지만 먼저 읽기
-    String beforeFullMsg = Split(fullMsg, ';', 0);
-
-    /////// 국가코드 READ 후 국가코드에 맞는 폰트를 설정한다 /////// 
-    int langCodeInt = Split(beforeFullMsg, ':', 0).toInt(); 
-    if(previousLangCode != langCodeInt)
-    {
-      ChangeUTF(langCodeInt);
-      previousLangCode = langCodeInt;
-    }
-
-    /////// 메세지 READ 후 출력한다 ///////
-    String finalMsg = Split(beforeFullMsg, ':', 1);
-    Message(finalMsg);
-  }
-  delay(20);
+  u8g2.sendBuffer();
 }
 
 void ChangeUTF(int langCodeInt)
@@ -125,101 +269,3 @@ void ChangeUTF(int langCodeInt)
       break;
   }
 }
-      
-
-String Split(String data, char separator, int index)
-{
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
-
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
-    }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
-}
-
-void Message(String msg){
-
-  u8g2.clearBuffer();
-  int lineLength = 15;
-  int len = msg.length();
-  int lines = len / lineLength + 1;
-  int height = 10;
-  for (int i = 0; i < lines; i++) {
-  int end = min((i + 1) * lineLength, len);
-  String line = msg.substring(i * lineLength, end);
-  u8g2.drawStr(0, height, line.c_str());
-  height += 10;
-  }
-
-  u8g2.sendBuffer();
-}
-
-
-
-
-
-
-//ETC
-
-//u8g2.setFont(u8g2_font_unifont_h_utf8);
-//u8g2.setFont(u8g2_font_unifont_t_korean1); 
-//u8g2.setFont(u8g2_font_unifont_t_korean1); 
-//u8g2.setFont(u8g2_font_b10_t_japanese1);
-//u8g2.setFont(u8g2_font_unifont_h_utf8); 
-//u8g2.setFont(u8g2_font_unifont_t_korean_NanumGothicCoding_16);
-//u8g2.setFont(u8g2_font_unifont_t_chinese2); 
-//u8g2.setFont(u8g2_font_b10_t_japanese1); 
-//u8g2.setFont(u8g2_font_unifont_t_cjk);
-//u8g2.setFont(u8g2_font_unifont_t_arabic);//아랍어
-//u8g2.setFont(u8g2_font_unifont_t_polish);//폴란드어
-//u8g2.setFont(u8g2_font_unifont_t_vietnamese1);//베트남어
-//u8g2.setFont(u8g2_font_unifont_t_deutsche;//독일어
-//u8g2.setFont(u8g2_font_unifont_t_Portugiesisch1);
-//포르투칼어u8g2_font_unifont_tf
-
-//u8g2.setFont(u8g2_font_unifont_te);
-//u8g2.setFont(u8g2_font_unifont_t_latin);
-//u8g2.setFont(u8g2_font_unifont_t_extended);
-//u8g2.setFont(u8g2_font_unifont_t_greek);
-//u8g2.setFont(u8g2_font_unifont_t_cyrillic);
-//u8g2.setFont(u8g2_font_unifont_t_hebrew);
-//u8g2.setFont(u8g2_font_unifont_t_bengali);
-//u8g2.setFont(u8g2_font_unifont_t_tibetan0;
-//u8g2.setFont(u8g2_font_unifont_t_urdu):
-//u8g2.setFont(u8g2_font_unifont_t_devanagari)
-//u8g2.setFont(u8g2_font_unifont_t_symbols);
-//u8g2.setFont(u8g2_font_unifont_t_emticons);
-//u8g2.setFont(u8g2_font_unifont_t_animals);
-//u8g2.setFont(u8g2_font_unifont_t_chinese1);
-//u8g2.setFont(u8g2_font_unifont_t_chinese2);
-//u8g2.setFont(u8g2_font_unifont_t_chinese3);
-//u8g2.setFont(u8g2_font_unifont_t_korean1);
-//u8g2.setFont(u8g2_font_unifont_t_korean2);
-//u8g2.setFont(u8g2_font_unifont_t_korean3);
-//u8g2.setFont(u8g2_font_unifont_t_vietnamese1);
-//u8g2.setFont(u8g2_font_unifont_t_vietnamese2);
-//u8g2.setFont(u8g2_font_unifont_t_cjk);
-
-
-// u8g2.setCursor(0, 32);
-
-//u8g2.print("Hello World!");
-//u8g2.print("مرحبا، انا سعيد لرؤيتك");
-//u8g2.print("你好, 世界!");
-// u8g2.print("어서오세요,반갑습니다");
-//u8g2.print("こんにちは世界!");
-//u8g2.print("Cześć miło");//폴란드어
-//u8g2.print("xin chào");//베트남어
-// u8g2.print("Hola mucho gusto");스페인어
-//u8g2.print("Schön dich kennenzulernen");//독일어
-//u8g2.print("Prazer em conhecê-la");//포르투칼어
-//u8g2.print("Piacere di conoscerti");//이탈리아
-
-//   u8g2.sendBuffer();
-  
